11f1e12eda879052e747f874cfe44d97942dfe30
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-07-29 19:14:45 +0530

    Added increment and decrement Buttons.

--- app/src/main/java/pramod/com/scorekeeper/MainActivity.java
+++ app/src/main/java/pramod/com/scorekeeper/MainActivity.java
@@ -3,6 +3,8 @@ package pramod.com.scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void decrementScore(View view) {
+        Toast.makeText(this, "Decrement is Pressed", Toast.LENGTH_SHORT).show();
+    }
+
+    public void incrementScore(View view) {
+        Toast.makeText(this, "Increment is Pressed", Toast.LENGTH_SHORT).show();
     }
 }
