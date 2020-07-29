f4d0be428e1992f5398f743fa7e97886835bdff4
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-07-29 19:25:23 +0530

    Added textView and count variables.

--- app/src/main/java/pramod/com/scorekeeper/MainActivity.java
+++ app/src/main/java/pramod/com/scorekeeper/MainActivity.java
@@ -4,21 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.result);
     }
 
     public void decrementScore(View view) {
         Toast.makeText(this, "Decrement is Pressed", Toast.LENGTH_SHORT).show();
+        count--;
+        tv.setText(count);
     }
 
     public void incrementScore(View view) {
         Toast.makeText(this, "Increment is Pressed", Toast.LENGTH_SHORT).show();
+        count++;
+        tv.setText(count);
     }
 }
